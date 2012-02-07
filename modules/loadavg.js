3adb6e5044f34b51582e6220962688453672411f
natrim <natrim_cz@yahoo.com>
2012-02-07 16:09:54 +0100

    fix loadavg

--- modules/loadavg.js
+++ modules/loadavg.js
@@ -7,7 +7,9 @@ exports.init = function(bot, dispatcher, loadavg) {
 			fs.readFile("/proc/loadavg", function(err, data) {
 				if (err)
 					source.mention("i don't know :(");
-				else source.mention(data.replace(/\n/g, ""));
+				else if(typeof data == "string")
+					source.mention("load average is: " + data.replace(/\n/g, ""));
+				else source.mention("load average is: " + data.toString().replace(/\n/g, ""));
 			});
 		} },
 	]);
