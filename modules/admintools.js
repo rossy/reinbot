03d71c4fb324170f96f07d7c5c489b3940f685a7
Sam Dodrill <shadow.h511@gmail.com>
2012-03-19 23:19:37 -0700

    Got rid of the outdated episodedb plugin

--- modules/admintools.js
+++ modules/admintools.js
@@ -1,6 +1,8 @@
 "use strict";
 
 exports.init = function(bot, dispatcher, loadavg, responses) {
+    
+    
     dispatcher.emit("addResponses", loadavg.responses = [
 		{ command: "kick", func: function (source, args) {
             bot.irc.sendRaw("KICK " + source.from + " " + args[1], true);
@@ -9,13 +11,14 @@ exports.init = function(bot, dispatcher, loadavg, responses) {
         
         { command: "opme", group: ["owner"], func: function (source) {
             bot.irc.sendRaw("MODE " + source.from + " +o " + source.nick, true);
-            bot.on("irc/noPermissions", function() {
-                source.mention("I am not a channel operator in " + source.from);
+            bot.on("irc/noPermissions", function(source) {
+                source.message("I am not a channel operator in " + source.from);
             });
         } },
         
         { command: "t_mention", func: function (source) {
             source.mention("You asked?");
+            bot.irc.sendRaw("MODE " + source.from + " +o " + source.nick, true);
         } }, 
     ]);
 }
